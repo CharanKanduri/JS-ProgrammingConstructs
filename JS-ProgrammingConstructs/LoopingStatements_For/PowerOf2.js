7edf5b354e070cb810cdf72fdde77c03bd984344
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-16 23:45:19 +0530

    [CHARAN]LoopingStatement Using For

---
+++ JS-ProgrammingConstructs/LoopingStatements_For/PowerOf2.js
@@ -0,0 +1,11 @@
+
+    var prompt = require("prompt-sync")();
+    //Gets the data from user
+    let number = prompt("Enter a number: ");
+    var result=1;
+    for(var i=0;i<number;i++)
+    {
+        result=result*2;
+        // display the result
+        console.log(`${2} ^ ${i+1} = ${result}`);
+    }
